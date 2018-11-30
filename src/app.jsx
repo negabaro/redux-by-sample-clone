90dcb24aea32f79823e53ab48125fe69fbe648ea
negabaro kim <negabaro@naver.com>
2018-11-30 13:11:25 +0000

    add redux

---
+++ src/app.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+/* Actionsの実装 */
+
+// Action名の定義
+const SEND = 'SEND';
+
+// Action Creators
+function send(value) {
+  // Action
+  return {
+    type: SEND,
+    value,
+  };
+}
+
+/* Reducersの実装 */
+
+function formReducer(state, action) {
+  switch (action.type) {
+    case 'SEND':
+      return Object.assign({}, state, {
+        value: action.value,
+      });
+    default:
+      return state;
+  }
+}
+
+/* Storeの実装 */
+
+const initialState = {
+  value: null,
+};
+const store = createStore(formReducer, initialState);
+
+/* Viewの実装 */
+
+// View (Container Components)
+class FormApp extends React.Component {
+  render() {
+    
+    console.log("this.props",this.props);
+    console.log("this.props.onClick22",this.props.onClick22);
+    //mapDispatchToProps -> onClick22(value) { -> this.props.onClick22
+    return (
+      <div>
+        <FormInput handleClick={this.props.onClick22} />
+        <FormDisplay data={this.props.value} />
+      </div>
+    );
+  }
+}
+FormApp.propTypes = {
+  onClick22: React.PropTypes.func.isRequired,
+  value: React.PropTypes.string,
+};
+
+// View (Presentational Components)
+class FormInput extends React.Component {
+  send22(e) {
+    e.preventDefault();
+    console.log("this.myInput.value.trim():", this.myInput.value.trim() );
+    console.log("this.props.handleClick",this.props.handleClick);
+    this.props.handleClick(this.myInput.value.trim());
+    this.myInput.value = '';
+    return;
+  }
+  render() {
+    return (
+      <form>
+        hh2
+        
+        <input type="text" ref={(ref) => (this.myInput = ref)} defaultValue="" />
+        <button onClick={(event) => this.send22(event)}>Send</button>
+      </form>
+    );
+  }
+}
+FormInput.propTypes = {
+  handleClick: React.PropTypes.func.isRequired,
+};
+
+// View (Presentational Components)
+class FormDisplay extends React.Component {
+  render() {
+    return (
+      <div>{this.props.data}</div>
+    );
+  }
+}
+FormDisplay.propTypes = {
+  data: React.PropTypes.string,
+};
+
+// Connect to Redux
+function mapStateToProps(state) {
+  window.console.log(state);
+  return {
+    value: state.value,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick22(value) {
+      dispatch(send(value));
+    },
+  };
+}
+const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FormApp);
+
+// Rendering
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.querySelector('.content')
+);
